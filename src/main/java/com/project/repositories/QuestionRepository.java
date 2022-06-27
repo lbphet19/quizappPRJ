56671feb1c2942242902457dda21c7d56ceb5ade
Minh Thang <lbphet19@gmail.com>
2022-06-28 00:13:57 +0700

    take back controller and repo

---
+++ src/main/java/com/project/repositories/QuestionRepository.java
@@ -0,0 +1,37 @@
+package com.project.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.project.entity.Answer;
+import com.project.entity.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer>{
+	@Query(value = "SELECT q FROM Question q"
+			+ " WHERE q.quiz.quizId = ?1")
+	List<Question> findQuestionByQuizId(int quizId);
+	
+	//test
+	@Query(value = "SELECT ans FROM Question q JOIN q.answers ans WHERE q.quiz.quizId = ?1")
+	List<Answer> findCorrectAnswers(int quizId);
+	
+	@Query(value = "SELECT DISTINCT q"
+			+ " FROM Question q"
+			+ " JOIN FETCH q.answers ans"
+			+ " WHERE q.quiz.quizId = ?1 AND ans.answerIsCorrect = true"
+			+ " ORDER BY q.questionId")
+	List<Question> getCorrectAnswers(int quizId);
+
+
+	@Query(value = "SELECT DISTINCT q"
+	+ " FROM Question q"
+	+ " JOIN FETCH q.answers ans"
+	+ " WHERE q.questionId IN (SELECT q.questionId FROM ExamQuestion exQuest JOIN"
+	+ " exQuest.exam ex JOIN exQuest.question q"
+	+ "	WHERE ex.examId = ?1 )"
+	+ " AND ans.answerIsCorrect = true"
+	+ " ORDER BY q.questionId")
+	List<Question> getCorrectAnswersFromExam(int examId);
+}
