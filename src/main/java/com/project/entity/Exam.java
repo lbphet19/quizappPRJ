c8c2bcb96f8af09050d1976af54075a9fb29b5e1
Minh Thang <lbphet19@gmail.com>
2022-07-30 01:15:15 +0700

    add anno

--- src/main/java/com/project/entity/Exam.java
+++ src/main/java/com/project/entity/Exam.java
@@ -9,7 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
